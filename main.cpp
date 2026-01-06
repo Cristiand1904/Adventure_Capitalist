9e969990bc34f282979995a1f72547422008fb9e
Cristiand1904 <ganesanucristian@gmail.com>
2026-01-06 23:44:49 +0200

    tema2 v0.2

--- main.cpp
+++ main.cpp
@@ -12,40 +12,6 @@ int main() {
     Application app;
     std::cout << "Aplicatie initializata. Rulare...\n";
 
-#ifdef USE_HEADLESS_APP
-    try {
-        auto& game = app.getGame();
-        game.getPlayer();
-        game.saveGame("test.txt");
-        game.loadGame("test.txt");
-        game.saveFileExists("test.txt");
-
-        auto& player = game.getPlayer();
-        const auto& businesses = player.getBusinesses();
-        if (!businesses.empty()) {
-            player.startBusinessProduction(0);
-            try {
-                player.purchaseBusiness(0);
-            } catch (...) {}
-            try {
-                player.upgradeBusiness(0);
-            } catch (...) {}
-        }
-
-        const auto& achievements = player.getAchievements();
-        if (!achievements.empty()) {
-            achievements[0].getDescription();
-        }
-
-        if (!businesses.empty()) {
-            businesses[0]->setManagerHired(false);
-            businesses[0]->getProgress();
-            businesses[0]->getProductionTime();
-            businesses[0]->isActive();
-        }
-    } catch (...) {}
-#endif
-
     app.run();
 
     std::cout << "Aplicatie terminata cu succes.\n";
